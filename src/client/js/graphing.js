eacc7ce672dece0675d0031d57e2d20a46c40abf
BuildTools <22253494@student.uwa.edu.au>
2022-09-04 18:31:47 +0800

    basic d3 setup

--- src/client/js/graphing.js
+++ src/client/js/graphing.js
@@ -21,56 +21,63 @@ let graph = {
     ]
 }
 
+//? Simulation settings
 let simulation = d3
-.forceSimulation(graph.nodes)
-.force("link", d3.forceLink(graph.links).id(function(d) {
-    return d.name;
-})
-).force("charge", d3.forceManyBody().strength(-30))
-.force("center", d3.forceCenter(width / 2, height / 2))
-.on("tick", ticked);
+    .forceSimulation(graph.nodes)
+    .force("link", d3.forceLink().id(function (d) {
+        return d.name;
+    })
+    .links(graph.links)
+    )
+    .force("charge", d3.forceManyBody().strength(-300))
+    .force("center", d3.forceCenter(width / 2, height / 2))
+    .on("tick", ticked);
 
+//? Link Elements
 let link = svg
-.append("g")
-.selectAll("line")
-.data(graph.links)
-.enter()
-.append("line")
-.attr("stroke-width", function(d) {
-    return 3;
-})
-.style("stroke", "pink");
+    .append("g")
+    .attr("class", "links")
+    .selectAll("line")
+    .data(graph.links)
+    .enter()
+    .append("line")
+    .attr("stroke-width", function (d) {
+        return 4;
+    })
+    .style("stroke", "pink");
 
+//? Node Elements
 let node = svg
-.append("g")
-.selectAll("circle")
-.data(graph.nodes)
-.enter()
-.append("circle")
-.attr("r", 5)
-.attr("fill", function(d) {
-    return "orange"
-})
-.attr("stroke", "yellow");
+    .append("g")
+    .attr("class", "nodes")
+    .selectAll("circle")
+    .data(graph.nodes)
+    .enter()
+    .append("circle")
+    .attr("r", 6)
+    .attr("fill", function (d) {
+        return "orange"
+    })
+    .attr("stroke", "yellow");
 
 function ticked() {
-    link.attr("x1", function(d) {
+    link.attr("x1", function (d) {
         return d.source.x;
     })
-    .attr("y1", function(d) {
-        return d.source.y;
-    })
-    .attr("x2", function(d) {
-        return d.target.x;
-    })
-    .attr("y2", function(d) {
-        return d.target.y;
-    })
+        .attr("y1", function (d) {
+            return d.source.y;
+        })
+        .attr("x2", function (d) {
+            return d.target.x;
+        })
+        .attr("y2", function (d) {
+            return d.target.y;
+        })
 
-    node.attr("cx", function(d) {
+    node.attr("cx", function (d) {
         return d.x;
     })
-    .attr("cy", function(d) {
-        return d.y;
-    });
+        .attr("cy", function (d) {
+            return d.y;
+        });
 }
