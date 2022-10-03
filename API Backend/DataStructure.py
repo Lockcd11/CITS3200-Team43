ab703b8fd498e74d5b28472fb77448f3e4e7b257
Lockcd11 <CameronDLocke@hotmail.com>
2022-10-03 13:34:17 +0800

    Update DataStructure.py
    Updated CSV generators, increased maximum number of co-authors per researcher, and max researchers per publication (step by step to 25 from 10).
    
    Stability upgrades. Fixed issue causing 2nd degree contacts not to be put in csvs

--- API Backend/DataStructure.py
+++ API Backend/DataStructure.py
@@ -64,13 +64,16 @@ while depth < 2:
         researcherQueue.pop(0)
         try:
             researcherFile = AuthorRetrieval(thisResearcher)
-            researchers_fullinfo.append([researcherFile.identifier, researcherFile.given_name+' '+researcherFile.surname, 'https://www.scopus.com/authid/detail.uri?authorId='+str(researcherFile.identifier), depth])     
+            researchers_fullinfo.append([researcherFile.identifier, researcherFile.given_name+' '+researcherFile.surname, 'https://www.scopus.com/authid/detail.uri?authorId='+str(researcherFile.identifier),researcherFile.coauthor_count, depth])     
             thisAuthorPublications = pd.DataFrame(researcherFile.get_document_eids())
-            if researcherFile.coauthor_count < 200:
+            if researcherFile.coauthor_count < 250:
                 for x in thisAuthorPublications[0]:
                     if x not in publications:
                         publicationQueue.append(x)
                     publications.add(x)
+            else:
+                publications.add(x)
+                relationships.add(x, researcherFile.identifier)
         except:
             researcherErrors.append(str(thisResearcher))
             for each in coreTeam:
@@ -81,20 +84,36 @@ while depth < 2:
         publicationQueue.pop(0)
         print(len(publicationQueue))
         pubretrieval=AbstractRetrieval(thisPublication)
-        pubs_fullinfo.append([pubretrieval.identifier, pubretrieval.title, pubretrieval.subtype, pubretrieval.publisher, pubretrieval.scopus_link])
         thisPublicationAuthors = pd.DataFrame(pubretrieval.authors)
-        if len(thisPublicationAuthors.iloc[:,0])<15:
+        pubs_fullinfo.append([pubretrieval.identifier, pubretrieval.title, len(thisPublicationAuthors.iloc[:,0]), pubretrieval.subtypedescription, pubretrieval.publisher, pubretrieval.scopus_link])
+        if len(thisPublicationAuthors.iloc[:,0])<25:
+            author=0
             for x in thisPublicationAuthors.iloc[:,0]:
-                relationships.add((thisPublication, x))
-                if depth !=2:
-                    if x not in researchers:
-                        researcherQueue.append(x)
-                    researchers.add(x)
+                relationships.add((pubretrieval.identifier, x))
+                if depth==1 and x not in researchers:
+                    try:
+                        researchers_fullinfo.append([x, thisPublicationAuthors.iloc[:,3][author]+' '+thisPublicationAuthors.iloc[:,2][author],'https://www.scopus.com/authid/detail.uri?authorId='+str(x),None ,depth+1])
+                    except:
+                        researcherErrors.append(x)
+                if x not in researchers:
+                    researcherQueue.append(x)
+                researchers.add(x)
+                author+=1
+        else:
+            author=0
+            for x in thisPublicationAuthors.iloc[:,0]:
+                relationships.add((pubretrieval.identifier, x))
+                if depth==1 and x not in researchers:
+                    try:
+                        researchers_fullinfo.append([x, thisPublicationAuthors.iloc[:,3][author]+' '+thisPublicationAuthors.iloc[:,2][author],'https://www.scopus.com/authid/detail.uri?authorId='+str(x),None ,depth+1])
+                    except:
+                        researcherErrors.append(x)
+                author+=1
     depth=depth+1
     print('depthup')
 with open('relationships.csv', 'w', newline='') as out:
     csv_out=csv.writer(out)
-    csv_out.writerow(['publication eid', 'author id'])
+    csv_out.writerow(['publication_eid', 'author_id'])
     for x in relationships:
         csv_out.writerow(x)
 # with open('researchers.csv', 'w') as out: #Deprecated, was for printing just the researcher ids into a csv for testing
@@ -107,14 +126,14 @@ with open('relationships.csv', 'w', newline='') as out:
 #     csv_out.writerow(['publication'])
 #     for x in publications:
 #         csv_out.writerow([x])
-with open('fullresearchers.csv', 'w', newline='') as out:
+with open('researchers.csv', 'w', newline='') as out:
     csv_out=csv.writer(out)
-    csv_out.writerow(['id', 'name', 'link', 'degrees'])
+    csv_out.writerow(['id', 'name', 'link', 'coauthor_count' ,'degrees'])
     for x in researchers_fullinfo:
         csv_out.writerow(x)
-with open('fullpublications.csv', 'w', newline='') as out:
+with open('publications.csv', 'w', newline='') as out:
     csv_out=csv.writer(out)
-    csv_out.writerow(['id', 'title', 'type', 'publisher', 'link'])
+    csv_out.writerow(['id', 'title', 'author_count', 'type', 'publisher', 'link'])
     for x in pubs_fullinfo:
         csv_out.writerow(x)
 if len(researcherErrors)>0:
