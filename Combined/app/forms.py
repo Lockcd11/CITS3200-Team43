ce8bdbc2c042c845ceaf5dce9a0d9d8686bb117d
Lockcd11 <CameronDLocke@hotmail.com>
2022-10-12 14:25:50 +0800

    Update forms.py
    added senior tag

--- Combined/app/forms.py
+++ Combined/app/forms.py
@@ -1,9 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField
+from wtforms import StringField, IntegerField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 class AddResearcherForm(FlaskForm):
     researcher = IntegerField('Scopus ID', validators=[DataRequired()])
+    senior = BooleanField('Senior Researcher')
     submit = SubmitField('Submit')
 
 class ChangeAPIKey(FlaskForm):
