109483d3f5ad813199bfce0de7e59d162d4d50a3
MuhammadMaazAhmed <maazahmedpoke@gmail.com>
2022-10-13 23:13:02 +0800

    added animations and other ui tweaks

---
+++ Combined/app/static/js/test.js
@@ -0,0 +1,33 @@
+var particles = Particles.init({
+	selector: '.background',
+  // color: '#DA0463',
+  color: ['#DA0463', '#404B69', '#DBEDF3'],
+
+  // pauseOnBlur: true,
+  connectParticles: true,
+  sizeVariations: 5,
+  maxParticles: 150,
+  speed: 0.5,
+  // responsive: [
+  //   {
+  //     breakpoint: 768,
+  //     options: {
+  //       maxParticles: 200,
+  //       color: '#48F2E3',
+  //       connectParticles: false
+  //     }
+  //   }, {
+  //     breakpoint: 425,
+  //     options: {
+  //       maxParticles: 100,
+  //       connectParticles: true
+  //     }
+  //   }, {
+  //     breakpoint: 320,
+  //     options: {
+  //       maxParticles: 0 // disables particles.js
+  //     }
+  //   }
+  // ]
+  
+});
