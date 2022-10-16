b28370400acb38f495f02271088a381350c9efb7
Lockcd11 <CameronDLocke@hotmail.com>
2022-10-16 15:21:13 +0800

    Update forms.py

--- Combined/app/forms.py
+++ Combined/app/forms.py
@@ -1,12 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, BooleanField
+from wtforms import StringField, IntegerField, SubmitField, BooleanField, RadioField
 from wtforms.validators import DataRequired
 
 class AddResearcherForm(FlaskForm):
     researcher = IntegerField('Scopus ID', validators=[DataRequired()])
+    addremoveupdate = RadioField('Add / Remove / Update', choices=[('Add','Add Researcher'),('Remove','Remove Researcher'),('Update', 'Update Researcher')])
     senior = BooleanField('Senior Researcher')
     submit = SubmitField('Submit')
 
 class ChangeAPIKey(FlaskForm):
     APIKey = StringField('API Key', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class ExpandForm(FlaskForm):
+    researcher = IntegerField('Scopus Id', validators=[DataRequired()])
+    submit = SubmitField('Update - May take significant time')
